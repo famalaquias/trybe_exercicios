f159288eb0389572941f3f5b5537e1dc92f7aea9
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-02-23 10:03:14 -0300

    Adicionar alteraçoes feitas no exercicio pratico da Aula 01

--- fundamentos/bloco_06_HTML_CSS_Forms/dia_01_input_types_attributes/exercicio_da_aula_pratica/script.js
+++ fundamentos/bloco_06_HTML_CSS_Forms/dia_01_input_types_attributes/exercicio_da_aula_pratica/script.js
@@ -1,14 +1,44 @@
-const submitButton = document.querySelector('#submit-button');
+// Interrompa o comportamento padrão do botão submit utilizando o método preventDefault().
 function buttonSubmit(event) {
     event.preventDefault();
 }
 
 window.onload = function () {
-    submitButton.addEventListener('click', submitButton);
-}
+    const submitButton = document.querySelector('#submit-button');
+    submitButton.addEventListener('click', buttonSubmit);
+};
 
 
+// Crie um botão que limpe as informações contidas nos campos.
 function clearButton () {
     const clearButtonNow = document.querySelector('#submit-button-clear');
-    clearButtonNow.addEventListener('click', function(event))
+    clearButtonNow.addEventListener('click', function() {
+        const nomeCompleto = document.querySelector('#nome-completo');
+        nomeCompleto.value = '';
+        
+        const endereçoEmail = document.querySelector('#input-email');
+        endereçoEmail.value = '';
+
+        var destino1 = document.querySelector('#destino1');
+        destino1.checked = '';
+        var destino2 = document.querySelector('#destino2');
+        destino2.checked = '';
+        var destino3 = document.querySelector('#destino3');
+        destino3.checked = '';
+        var destino4 = document.querySelector('#destino4');
+        destino4.checked = '';
+
+        const textArea = document.querySelector('#textarea');
+        textArea.value = '';
+
+        const date = document.querySelector('#date');
+        date.value = '';
+
+        const checkboxOportunidades = document.querySelector('#checkbox-oportunidades');
+        checkboxOportunidades.checked = '';
+
+        const checkboxImagem = document.querySelector('#checkbox-imagem');
+        checkboxImagem.checked = '';
+    });
 }
+clearButton();
